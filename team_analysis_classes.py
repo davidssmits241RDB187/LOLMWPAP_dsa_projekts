c07e578f41e9f33eb93a87e60543ecc6324ee331
davidssmits241RDB187 <davids.smits@edu.rtu.lv>
2025-05-12 12:55:56 +0300

    adding some player role coefficient funtionality,  classes will prob need to be seperated into different files

--- team_analysis_classes.py
+++ team_analysis_classes.py
@@ -21,9 +21,10 @@ class Player:
         self.matches_played = 0
         self.player_evaluation += self.kda + self.kp + self.cspm + self.vspm + self.dmg
         
-        
+
+
 @dataclass
-class DefaultTeamValues():
+class DefaultTeamValues:
     winrate: dict[str, float] = field(default_factory=dict)
     gold_per_minute: float
     gold_differential_per_minute:float
@@ -51,6 +52,43 @@ class DefaultTeamValues():
     feats_of_strength:float
     
     vision_score_per_minute:float
+    
+    
+@dataclass
+class Coefficients:
+    gold_lead: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    kills: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    towers: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    dragons: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    barons: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    
+    top_kda: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    top_csm: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    top_dpm: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    top_wpm: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    
+    mid_kda: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    mid_csm: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    mid_dpm: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    mid_wpm: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    
+    jungle_kda: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    jungle_csm: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    jugnle_dpm: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    jungle_wpm: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    
+    bot_kda: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    bot_csm: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    bot_dpm: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    bot_wpm: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    
+    support_kda: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    support_csm: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    support_dpm: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    support_wpm: list[float] = field(default_factory=lambda: [1.0, 1.0])
+    
+    
+
 class Team:
     def __init__(self,players,winrate,gold_per_minute,gold_differential_per_minute, gold_differential_at_15_min, cspm, cs_differential_at_15_min,tower_differential_at_15_min, avg_tower_difference, first_tower, damage_per_minute, first_blood,kills_per_game,deaths_per_game,avg_kd,plates_per_game,plates_per_game_per_side,dragons_per_game,dragons_at_15_min,void_grubs_per_game,atakhan_per_game,herald_per_game,nashor_per_game,feats_of_strength):
         
@@ -104,7 +142,29 @@ class Team:
             player.vspm += player.vspm*match_coefficient
             player.dmg += player.dmg*match_coefficient
             player.player_evaluation += player.kda + player.kp + player.cspm + player.vspm + player.dmg
-    #!     
+    #!   
+    
+    
+    
+def evaluate_coefficients(self,winning_team,losing_team,read_coefficients):
+       i = 0
+       #kda csm dpm vpm
+       while i < 5:
+           if winning_team.players[i].kda>losing_team.players[i].kda:
+                if winning_team.players[i].role == "top":
+                   read_coefficients.top_kda[0] += 1
+                elif winning_team.players[i].role == "jungle":
+                   read_coefficients.jungle_kda[0] += 1
+                elif winning_team.players[i].role == "mid":
+                   read_coefficients.mid_kda[0] += 1
+                elif winning_team.players[i].role == "mid":
+                   read_coefficients.mid_kda[0] += 1
+                elif winning_team.players[i].role == "bot":
+                   read_coefficients.bot_kda[0] += 1
+                elif winning_team.players[i].role == "support":
+                   read_coefficients.support_kda[0] += 1
+           i += 1
+           
 class Match:
     def __init__(self, team1, team2):
         self.team1 = team1
@@ -124,7 +184,9 @@ class Match:
             self.team1.team_evaluation+=1
         elif value_for_team1<value_for_team2:
             self.team2.team_evaluation+=1
-        
+            
+            
+    
             
             
 
