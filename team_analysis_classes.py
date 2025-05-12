e444d62f0682fe928ae3b1008eb3bef59a9a9378
davidssmits241RDB187 <davids.smits@edu.rtu.lv>
2025-05-12 09:17:15 +0300

    working on essential team anlysis, individual match analysis can be worked on if time permits

--- team_analysis_classes.py
+++ team_analysis_classes.py
@@ -10,44 +10,79 @@ class Player:
         self.kp = kp
         self.cspm = cspm
         self.vspm = vspm
-        self.dmg = dmg
         
+        #!gol gg team statistics returns dmg and gold as percentages, which will not work with evaluation
+        #!only use these in individual match analysis
+        self.dmg = dmg
         self.gold = gold
+        #!
         
         self.champions_dictionary = champions_dictionary # champion name: win percentage (example-> janna:0.63)
         self.matches_played = 0
-        self.player_evaluation = 0
+        self.player_evaluation += self.kda + self.kp + self.cspm + self.vspm + self.dmg
         
 class Team:
-    def __init__(self):
-        self.players = []
-        self.gold_lead = 0
+    def __init__(self,players):
+        
+        #!this section is for live game analysis, not essential!
+        self.dragon_priority = {} #example {cloud: [11, 6], ocean: [8, 12]} first in list is killed, second in list is lost
         self.average_game_duration = 0
-        self.matches = 0
         self.side_winrates = [0,0]
+        #!
+        
+        self.winrate = 0
+        
+        self.gold_per_minute = 0
+        self.gold_differential_per_minute = 0
+        self.gold_differential_at_15_min = 0
+        self.cspm = 0
+        self.cs_differential_at_15_min = 0
+        self.tower_differential_at_15_min = 0
+        self.avg_tower_difference = 0
+        self.first_tower = 0
+        
+        self.damage_per_minute = 0
+        self.first_blood = 0
+        self.kills_per_game = 0
+        self.deaths_per_game = 0
+        self.avg_kd = 0
+        
+        self.plates_per_game = 0
+        self.plates_per_game_per_side = {} # example top:1.2, mid:0.7
+        self.dragons_per_game = 0
+        self.dragons_at_15_min = 0
+        self.void_grubs_per_game = 0
+        self.atakhan_per_game = 0
+        self.herald_per_game = 0
+        self.nashor_per_game = 0
+        self.feats_of_strength = 0
+        
+        self.vision_score_per_minute = 0
+        
+        self.players = players
         self.team_evaluation = 0
-        '''
-        self.top_laners = []
-        self.junglers=[]
-        self.mid_laners = []
-        self.adcs=[]
-        self.supports = []
+        self.matches = 0
+        self.top_laner = None
+        self.jungler = None
+        self.mid_laner = None
+        self.adc = None
+        self.support = None
         for player in self.players:
             Role = player.role
             match Role:
                 case "top":
-                    self.top_laners.append(player)
-                case "jungler":
-                    self.junglers.append(player)
+                    self.top_laner = player
+                case "jungle":
+                    self.jungler = player
                 case "mid":
-                    self.mid_laners.append(player)
-                case "adc":
-                    self.adcs.append(player)
+                    self.mid_laner = player
+                case "bot":
+                    self.adc = player
                 case "support":
-                    self.supports.append(player)
-        '''
+                    self.support = player
         
-       
+        
+    #!prototype for individual match analysis, not essential!
     def evaluate_team_match_and_tournament(self,tournament_name, tournament_kills,gold_lead,match_players):
         match_coefficient = 1
         
@@ -65,9 +100,8 @@ class Team:
             player.cspm += player.cspm*match_coefficient
             player.vspm += player.vspm*match_coefficient
             player.dmg += player.dmg*match_coefficient
-            player.self_evaluation += player.kda + player.kp + player.cspm + player.vspm + player.dmg
-            
-    
+            player.player_evaluation += player.kda + player.kp + player.cspm + player.vspm + player.dmg
+    #!     
 class Match:
     def __init__(self, team1, team2):
         self.team1 = team1
@@ -79,8 +113,10 @@ class Match:
         self.adc_coefficient = 1
         self.support_coefficient = 1
 
-    def evaluate_team1_vs_team2(self):
-        pass
+    def evaluate_team1_vs_team2_default(self):
+        if self.team1.top_laner.player_evaluation > self.team2.top_laner.player_evaluation:
+            self.team1.team_evaluation += 1*self.top_coefficient
+        
             
             
 
