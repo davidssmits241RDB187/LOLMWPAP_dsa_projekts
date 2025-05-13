3b554e930ba5746afff45a0520667b5c782b8194
davidssmits241RDB187 <davids.smits@edu.rtu.lv>
2025-05-14 00:47:40 +0300

    updated getting the winning and losing team coefficient attributes and winning and losing team winning and losing player attributes

--- evaluate_function.py
+++ evaluate_function.py
@@ -21,8 +21,8 @@ def evaluate_coefficients(winning_team:Team,losing_team:Team):
         team_coeffs = ["gold_lead","kills","towers","dragons","barons"]
         for x in range(5):
             
-            win_value = getattr(getattr(winning_team,"default_team_values"),default_team_values_list[x])
-            los_value = getattr(getattr(losing_team,"default_team_values"),default_team_values_list[x])
+            win_value = getattr(winning_team,default_team_values_list[x])
+            los_value = getattr(losing_team,default_team_values_list[x])
             if win_value > los_value:
                 getattr(read_coefficients,team_coeffs[x])[0]+=1
             elif win_value < los_value:
@@ -33,9 +33,11 @@ def evaluate_coefficients(winning_team:Team,losing_team:Team):
         players_coeffs = ["kda", "csm", "dpm", "wpm"]
 
         for i in range(5):
+            winning_player = getattr(winning_team,roles[i])
+            losing_player = getattr(losing_team,roles[i])
             for attribute in player_attributes:
-                win_val = getattr(winning_team.players[i], attribute)
-                lose_val = getattr(losing_team.players[i], attribute)
+                win_val = getattr(winning_player, attribute)
+                lose_val = getattr(losing_player, attribute)
                 coeff = f"{roles[i]}_{players_coeffs[player_attributes.index(attribute)]}"
                 if win_val > lose_val:
                     getattr(read_coefficients, coeff)[0] += 1
